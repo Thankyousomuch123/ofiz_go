49c65d839e92d4c9fc8b54b789ef9f3a400dc88f
Sangamesh Mugadur <sangameshbm71998@gmail.com>
2023-10-14 23:02:58 +0530

    deleting all models

--- db/migrate/20230909125226_create_attendances.rb
+++
@@ -1,9 +0,0 @@
-class CreateAttendances < ActiveRecord::Migration[7.0]
-  def change
-    create_table :attendances do |t|
-      t.time :login_time
-      t.time :logout_time
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230909130335_create_performances.rb
+++
@@ -1,9 +0,0 @@
-class CreatePerformances < ActiveRecord::Migration[7.0]
-  def change
-    create_table :performances do |t|
-      t.integer :performance_grade
-      t.string :feedback
-      t.timestamps
-    end
-  end
-end
